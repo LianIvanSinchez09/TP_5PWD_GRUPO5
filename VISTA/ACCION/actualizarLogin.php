e178460d54956180eff781a72bd00f02f3d5cef8
Tomas Mengon <tomaso200208@gmail.com>
2025-11-05 23:07:35 -0300

    cambios

--- VISTA/ACCION/actualizarLogin.php
+++ VISTA/ACCION/actualizarLogin.php
@@ -1,3 +1,17 @@
 <?php
 
+include_once "../../UTILS/funciones.php";
+
 $datos = data_submitted();
+$abmUsuario = new AbmUsuario();
+
+$usuario = ['id_usuario' => $datos['id_usuario']];
+
+$listaUsuario = $abmUsuario->buscar($usuario);
+$objUsuario = $listaUsuario[0];
+
+$datos['usuario_deshabilitado'] = $objUsuario->getUsuarioDeshabilitado();
+
+$abmUsuario->modificar($datos);
+
+header('Location: ../../index.php');
--- VISTA/ACCION/verificarLogin.php
+++ VISTA/ACCION/verificarLogin.php
@@ -5,7 +5,14 @@ include_once '../MODELO/Usuario.php';
 include_once '../../UTILS/funciones.php';
 
 $nuevoUsuarioDatos = data_submitted();
+$session = new Session();
+
+$session->iniciar($usuario['username'], $usuario['password']);
+if ($session->validar()) {
+    header('Location: ../paginaSegura.php');
+} else {
+    $message = "Error al iniciar sesion";
+    header('Location: ../../index.php?message=' .urlencode($message));
+}
 
 
-
-$usuario = new AbmUsuario();
